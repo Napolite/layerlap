5768eaaaaf94f0c0a6983dc049448501d9d6572f
Constantino <Okonemmanuelmma@gmail.com>
2023-05-26 23:20:49 +0100

    :feature : navbar compenent(jsx skeleton)

---
+++ src/assets/index.tsx
@@ -0,0 +1,4 @@
+import logo from "./images/layerlap 1.png";
+import layerLap from "./images/Vector.png";
+
+export { layerLap, logo };
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,19 @@
+import { layerLap } from "../../assets";
 function Navbar() {
   return (
     <div>
-      <div>Navbar</div>
+      <div>
+        <img src={layerLap} />
+      </div>
+      <div>
+        <div>Home</div>
+        <div>Services</div>
+        <div>Solutions</div>
+        <div>Contacts</div>
+      </div>
+      <div>
+        <button>Get Started</button>
+      </div>
     </div>
   );
 }
