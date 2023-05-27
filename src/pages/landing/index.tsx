9c12c57434ac9a4c67aaa2cdd6991ed49e32a41f
Constantino <Okonemmanuelmma@gmail.com>
2023-05-27 01:15:44 +0100

    :feature :landing page html skeleton

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -3,7 +3,16 @@ import { logo } from "../../assets";
 function Landing() {
   return (
     <div>
-      <div></div>
+      <div>
+        <div></div>
+        <div>
+          <div>SOlid Expertise In Development</div>
+          <div>
+            We deliver professional software development services for businesses
+            and projects.
+          </div>
+        </div>
+      </div>
       <div>
         <img src={logo} />
       </div>
