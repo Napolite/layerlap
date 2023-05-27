8190c0688fb9871dbc873e3fb65261a477489c9e
Constantino <Okonemmanuelmma@gmail.com>
2023-05-27 02:57:29 +0100

    :init: initialised services page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // import { useState } from 'react'
 import "./App.css";
 import { Navbar } from "./components";
-import { Landing } from "./pages";
+import { Landing, Services } from "./pages";
 
 function App() {
   return (
     <>
       <Navbar />
       <Landing />
+      <Services />
     </>
   );
 }
