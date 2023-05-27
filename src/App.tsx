9d483a74d77a0fe5139ef3ad29998f485bee14b3
Constantino <Okonemmanuelmma@gmail.com>
2023-05-27 01:12:38 +0100

    :misc : folder structure for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 import "./App.css";
 import { Navbar } from "./components";
+import { Landing } from "./pages";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Landing />
     </>
   );
 }
---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,14 @@
+import { logo } from "../../assets";
+
+function Landing() {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <img src={logo} />
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
