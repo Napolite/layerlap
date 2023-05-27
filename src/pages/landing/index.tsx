9509522f4ad506ea09285e6679c2f74d0025889c
Constantino <Okonemmanuelmma@gmail.com>
2023-05-27 02:37:18 +0100

    :feature :landing page css skeleton

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,12 +1,13 @@
 import { logo } from "../../assets";
+import "./landing.css";
 
 function Landing() {
   return (
-    <div>
-      <div>
-        <div></div>
-        <div>
-          <div>SOlid Expertise In Development</div>
+    <div className="landing-root">
+      <div className="landing-left">
+        <div className="landing-circle"></div>
+        <div className="landing-statement">
+          <div>Solid Expertise In Development</div>
           <div>
             We deliver professional software development services for businesses
             and projects.
