3de8a394a6d3a9df932aec7489a0ec346ac313f5
Constantino <Okonemmanuelmma@gmail.com>
2023-05-27 11:34:05 +0100

    included images to assets index

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,4 +1,23 @@
 import logo from "./images/layerlap 1.png";
 import layerLap from "./images/Vector.png";
+import blockchain from "./images/blockchain.png";
+import consult from "./images/cyber-security.png";
+import cyber from "./images/cyber-security.png";
+import design from "./images/design.png";
+import webdev from "./images/web-dev.png";
+import education from "./images/education.png";
+import enterprise from "./images/enterprise.png";
+import marketing from "./images/marketing.png";
 
-export { layerLap, logo };
+export {
+  layerLap,
+  logo,
+  blockchain,
+  cyber,
+  consult,
+  design,
+  webdev,
+  education,
+  enterprise,
+  marketing,
+};
