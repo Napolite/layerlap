13cc867b89168fcfc02d3932b83d9119d107f54c
Constantino <Okonemmanuelmma@gmail.com>
2023-05-29 23:43:31 +0100

    contact : a lot of distracted work

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,34 +2,37 @@ import "./contact.css";
 
 function Contact() {
   return (
-    <div>
-      <div>Contact Us</div>
-      <div>Bring your Project to Life with LAYERLAB</div>
-      <div>
-        <div className="contact-left-div">
-          <div>Contact Information</div>
-          <div>Say something to start the livechat</div>
-          <div>
+    <div className="contact-root">
+      <div className="contact-header">Contact Us</div>
+      <div className="contact-sub-header">
+        Bring your Project to Life with LAYERLAB
+      </div>
+      <div className="contact">
+        <div className="contact-left-panel">
+          <div className="contact-left-panel-info">Contact Information</div>
+          <div className="contact-live">
+            Say something to start the livechat
+          </div>
+          <div className="contact-info">
             <img />
             <div></div>
           </div>
-          <div>
+          <div className="contact-info">
+            <img />
+            <div></div>
+          </div>
+          <div className="contact-info">
             <img />
             <div></div>
           </div>
 
-          <div>
-            <img />
-            <div></div>
-          </div>
-
-          <div>
+          <div className="contact-info-footer">
             <img />
             <img />
             <img />
           </div>
         </div>
-        <div>
+        <div className="contact-form">
           <form>
             <input type="text" name="First" id="" />
             <input type="text" name="Last" id="" />
