e91d4928db6b0378c83f0d107d3ce436a6318dee
Constantino <Okonemmanuelmma@gmail.com>
2023-05-29 18:40:08 +0100

    contact : html skeleton

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,7 +1,46 @@
 import "./contact.css";
 
 function Contact() {
-  return <div></div>;
+  return (
+    <div>
+      <div>Contact Us</div>
+      <div>Bring your Project to Life with LAYERLAB</div>
+      <div>
+        <div className="contact-left-div">
+          <div>Contact Information</div>
+          <div>Say something to start the livechat</div>
+          <div>
+            <img />
+            <div></div>
+          </div>
+          <div>
+            <img />
+            <div></div>
+          </div>
+
+          <div>
+            <img />
+            <div></div>
+          </div>
+
+          <div>
+            <img />
+            <img />
+            <img />
+          </div>
+        </div>
+        <div>
+          <form>
+            <input type="text" name="First" id="" />
+            <input type="text" name="Last" id="" />
+            <input type="text" name="email" id="" />
+            <input type="text" name="company" id="" />
+            <input type="text" name="message" id="" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
