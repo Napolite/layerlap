2e7b15165e523acdcd141a4159191e9ff64259ea
Constantino <Okonemmanuelmma@gmail.com>
2023-05-29 11:41:03 +0100

    :component : help section

---
+++ src/pages/help/index.tsx
@@ -0,0 +1,16 @@
+function Help() {
+  return (
+    <div>
+      <div className="help-header">How Can We Help</div>
+      <div className="help-body">
+        We provide tailored end-to-end solutions leveraging blockchain
+        technology for developers, startups, enterprises, and governments, to
+        meet specific needs. This comprehensive process covers every detail from
+        initial ideation to post-production support & maintenance.
+      </div>
+      <button className="help-button">Contact Us</button>
+    </div>
+  );
+}
+
+export default Help;
